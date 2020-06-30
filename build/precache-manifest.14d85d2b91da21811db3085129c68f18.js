41df9a4bb2589bc1d20049feff2fa55c5e182f6d
usama danish <usamadanish22@gmail.com>
2020-06-30 06:07:29 -0700

    added build into branch

---
+++ build/precache-manifest.14d85d2b91da21811db3085129c68f18.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6ad459bbefcee462317adefa11bc032b",
+    "url": "/UsamaDanish/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "0aa8b727210ca2664648",
+    "url": "/UsamaDanish/monsters-rolodex/static/css/main.1c99ee08.chunk.css"
+  },
+  {
+    "revision": "482d3b0974c298f0d8e6",
+    "url": "/UsamaDanish/monsters-rolodex/static/js/2.a998f851.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/UsamaDanish/monsters-rolodex/static/js/2.a998f851.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0aa8b727210ca2664648",
+    "url": "/UsamaDanish/monsters-rolodex/static/js/main.ea0eefe2.chunk.js"
+  },
+  {
+    "revision": "7cff0cdd91251a7b98ca",
+    "url": "/UsamaDanish/monsters-rolodex/static/js/runtime-main.12cab700.js"
+  }
+]);
